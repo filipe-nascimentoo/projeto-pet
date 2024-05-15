51c4b0ba7e0a4feecd4a6f1972b4f90169ea84de
Luis Filipe dos Santos Nascimento <filipe_nascimentoo@hotmail.com>
2024-05-14 22:20:56 -0300

    configurando servidor

--- servidor.js
+++ servidor.js
@@ -3,22 +3,24 @@ const express = require('express')
 const app = express()
 const porta = 4000
 
+// define a pasta publica do projeto (public)
+app.use(express('public'))
+
 app.get('/', (req, res) => {
-    res.send('Pagina principal')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/login',(req, res) => {
-    res.send('Pagina de login')
+    res.sendFile(__dirname + '/public/login.html')
 })
 
 app.get('/adote',(req, res) => {
-    res.send('Pagina de adoção')
+    res.sendFile(__dirname + '/public/adote.html')
 })
 
 app.get('/cadastrar',(req, res) => {
-    res.send('Pagina de cadastro')
+    res.sendFile(__dirname + '/public/cadastro.html')
 })
-
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta http://localhost:${porta}`)
