4bae51ab05912fe32534964aebd27904a12283d8
Luis Filipe dos Santos Nascimento <filipe_nascimentoo@hotmail.com>
2024-05-22 19:39:17 -0300

    ajustes js

--- servidor.js
+++ servidor.js
@@ -27,6 +27,31 @@ app.get('/login/cadastrar', (req, res) => {
     res.sendFile(__dirname + '/public/login-cadastrar.html')
 })
 
+app.get('/usuarios/listar/:id?', (req, res) => {
+
+    let id = req.params.id
+
+    const usuarios = [
+        {
+            nome: "Filipe",
+            telefone: "(19) 9 8106-8804",
+            email: "filipe.nascii@gmail.com"
+        },
+        {
+            nome: "Luis",
+            telefone: "(19) 9 8106-8805",
+            email: "filipe_nascimentoo@hotmail.com"
+        },
+        {
+            nome: "Giulliane",
+            telefone: "(19) 9 8278-2527",
+            email: "giuxavierdacruz@hotmail.com"
+        }
+    ]
+
+    id ? res.json(usuarios[id]) : res.json(usuarios)
+})
+
 app.get('/pets', (req, res) => {
     res.sendFile(__dirname + '/public/pets.html')
 })
@@ -35,12 +60,56 @@ app.get('/pets/cadastrar', (req, res) => {
     res.sendFile(__dirname + '/public/pets-cadastrar.html')
 })
 
+app.get('/pets/listar', (req, res) => {
+    // const pets = [
+    //     'Caramelo',
+    //     'Pitoco',
+    //     'Pocoto'
+    // ];
+
+    const pets = [
+        {
+            nome: "Caramelo",
+            especie: "Cachorro",
+            idade: 2
+        },
+        {
+            nome: "Chaninho",
+            especie: "Gato",
+            idade: 3
+        },
+        {
+            nome: "Pocoto",
+            especie: "Cavalo",
+            idade: 5
+        }
+    ]
+
+    res.json(pets)
+})
+
 
 // Define as rotas POST
 app.post('/login/cadastrar', (req, res) => {
     var { nome, telefone, email, senha, confirmar } = req.body
 
+    nome == '' ? res.send(`Campo nome é obrigatorio`) : ``;
+    senha != confirmar ? res.send(`O campo senha e confirmar senha estão diferentes`) : ``;
+
     res.send(`${nome} seu cadastro foi realizado com sucesso`)
+})
+
+app.post('/pets/cadastrar', (req, res) => {
+    var { nome, especie, idade } = req.body
+
+    nome == '' || idade == '' || especie == '' ? res.send(`Preencha todos os campos obrigatorios`) : ``;
+
+    res.send(`
+        <h1>Cadastro realizado com sucesso!</h1>
+        Nome: ${nome}<br>
+        Especie: ${especie}<br>
+        Idade: ${idade}
+    `)
 })
 
 
