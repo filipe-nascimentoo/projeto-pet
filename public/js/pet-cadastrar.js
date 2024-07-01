fbe100bb03ff73ae188bf3c8d57254236f32e91b
Luis Filipe dos Santos Nascimento <filipe_nascimentoo@hotmail.com>
2024-07-01 17:40:14 -0300

    Ajuste datatable pets

--- public/js/pet-cadastrar.js
+++ public/js/pet-cadastrar.js
@@ -86,7 +86,18 @@ const listaPet = () => {
                 </tr>
              `
             }
-            table = new DataTable('#pets');
+            table = new DataTable('#pets', {
+                scrollX: true,
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+                },
+                columnDefs: [
+                    {
+                        targets: 4,
+                        render: DataTable.render.date()
+                    }
+                ]
+            });
         })
         .catch(error => {
             Swal.fire({
